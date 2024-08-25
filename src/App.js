7677ec50ec13526291a92874f3dd37f6e75fe95c
artemkulinskii <a.kulinscky1@gmail.com>
2024-08-25 17:27:58 +0300

    prettier fixes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect, useState} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 import Header from "./Components/Header/Header";
 import Navbar from "./Components/NavBar/Navbar";
 import Profile from "./Components/Profile/Profile";
 
-
 const App = () => {
   // const [count, setCount] = useState(0)
   return (
-      <div className='app-wrapper'>
-          <Header/>
-          <Navbar/>
-          <Profile/>
-      </div>
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      <Profile />
+    </div>
   );
-}
-
-
+};
 
 export default App;
--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,15 +1,15 @@
-  import React from "react";
-import classes from './Profile.module.css'
-  import MyPosts from "./MyPosts/MyPosts";
+import React from "react";
+import classes from "./Profile.module.css";
+import MyPosts from "./MyPosts/MyPosts";
 const Profile = () => {
-    return <div className={classes.content}>
-        <img src='https://haleakalaecotours.com/wp-content/uploads/2019/04/hawaii-dangerous-waves.jpg'/>
+  return (
+    <div className={classes.content}>
+      <img src="https://haleakalaecotours.com/wp-content/uploads/2019/04/hawaii-dangerous-waves.jpg" />
 
-        <div>
-            ava+description
-        </div>
-       <MyPosts/>
+      <div>ava+description</div>
+      <MyPosts />
     </div>
-}
+  );
+};
 
-  export default Profile;
+export default Profile;
